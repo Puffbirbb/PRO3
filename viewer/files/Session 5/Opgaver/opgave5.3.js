e74f5fecfc179cfea26b0f79cff2fc17af2613fc
Puffbirbb <eaagmhc@students.eaaa.dk>
2023-02-14 09:17:50 +0100

    Session 5 post

---
+++ viewer/files/Session 5/Opgaver/opgave5.3.js
@@ -0,0 +1,9 @@
+while (document.querySelector("span").onclick) {
+    if (document.querySelector("span").onclick.value === "[Tab1]"){
+        
+    } else if (document.querySelector("span").onclick.value === "[Tab2]"){
+        this.display = hide;
+    } else if (document.querySelector("span").onclick.value === "[Tab3]"){
+        this.display = hide;
+    }
+}

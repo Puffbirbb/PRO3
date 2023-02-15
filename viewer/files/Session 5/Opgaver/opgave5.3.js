ea31b41b793dff4a8a4cccbbc8b1f7858a5ce7af
Puffbirbb <eaagmhc@students.eaaa.dk>
2023-02-15 22:37:09 +0100

    Session 5 post edit 1

--- viewer/files/Session 5/Opgaver/opgave5.3.js
+++ viewer/files/Session 5/Opgaver/opgave5.3.js
@@ -1,9 +1,19 @@
-while (document.querySelector("span").onclick) {
-    if (document.querySelector("span").onclick.value === "[Tab1]"){
-        
-    } else if (document.querySelector("span").onclick.value === "[Tab2]"){
-        this.display = hide;
-    } else if (document.querySelector("span").onclick.value === "[Tab3]"){
-        this.display = hide;
+let h1 = document.querySelector("#h1")
+let h2 = document.querySelector("#h2")
+let h3 = document.querySelector("#h3")
+
+let tab1 = document.querySelector("#tab1")
+tab1.onclick = () => hideOtherElements(h1);
+let tab2 = document.querySelector("#tab2")
+tab2.onclick = () => hideOtherElements(h2);
+let tab3 = document.querySelector("#tab3")
+tab3.onclick = () => hideOtherElements(h3);
+
+function hideOtherElements(element) {
+    let h = document.querySelectorAll("h1");
+    for (const e of h) {
+        e.style.display = e !== element ? "none" : "flex"
     }
 }
+
+hideOtherElements(h1);

c24383f09a00dbf35aa9ec337612f5cdd6bfe10f
Puffbirbb <eaagmhc@students.eaaa.dk>
2023-03-27 11:10:20 +0200

    Session 15 post edit 1

--- viewer/files/Session 15/Opgaver/opgave15.4.mjs
+++ viewer/files/Session 15/Opgaver/opgave15.4.mjs
@@ -21,10 +21,42 @@ const users = [
     email: "BRUHMOMENT@FRFR.CAP",
     nationality: "DK",
   },
-  // Generate 10 more users with a random nationality.
 ];
 
-// Write a function that creates 100 users with a random nationality
+const nationalities = [
+  "US",
+  "CA",
+  "FR",
+  "DE",
+  "GB",
+  "IT",
+  "JP",
+  "KR",
+  "CN",
+  "AU",
+  "NZ",
+  "RU",
+  "ES",
+  "SE",
+  "NO",
+  "DK",
+  "FI",
+];
+
+const randomNationality = () => {
+  const randomIndex = Math.floor(Math.random() * nationalities.length);
+  return nationalities[randomIndex];
+};
+
+for (let i = 0; i < 97; i++) {
+  const user = {
+    name: `User ${i + 1}`,
+    age: Math.floor(Math.random() * 70) + 18, // age between 18 and 87
+    email: `user${i + 1}@example.com`,
+    nationality: randomNationality(),
+  };
+  users.push(user);
+}
 
 let data = { useres: users };
 
@@ -32,7 +64,7 @@ app.set("view engine", "pug");
 app.set("views", "viewer\\files\\Session 15\\Opgaver");
 
 app.get("/", (req, res) => {
-  res.render("opgave15.2.pug", data);
+  res.render("opgave15.4.pug", data);
 });
 
 app.listen(8080, () => {
